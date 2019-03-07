2779cb7f888f9bd26ae87675ff1e163510fa4d72
hugodessomme <hdessomme@gmail.com>
2019-03-07 11:19:57 +0100

    Add dev/template for Home and Property

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\PropertyRepository;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     * @param PropertyRepository $repository
+     * @return Response
+     */
+    public function index(PropertyRepository $repository): Response
+    {
+        $properties = $repository->findLatest();
+
+        return $this->render('pages/home.html.twig', [
+            'properties' => $properties
+        ]);
+    }
+}
---
+++ src/Controller/PropertyController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Property;
+use App\Repository\PropertyRepository;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class PropertyController extends AbstractController
+{
+    /**
+     * @var PropertyRepository
+     */
+    private $repository;
+
+    /**
+     * @var ObjectManager
+     */
+    private $em;
+    
+    public function __construct(PropertyRepository $repository, ObjectManager $em)
+    {
+        $this->repository = $repository;
+        $this->em = $em;
+    }
+
+    /**
+     * @Route("/biens/", name="property.index")
+     * 
+     * @return Response
+     */
+    public function index(): Response
+    {
+        return $this->render('property/index.html.twig', [
+            'current_menu' => 'properties'
+        ]);
+    }
+
+    /**
+     * @Route("/biens/{slug}-{id}", name="property.show", requirements={"slug": "[a-z0-9\-]*"})
+     */
+    public function show(Property $property, string $slug): Response
+    {        
+        if ($property->getSlug() !== $slug) {
+            return $this->redirectToRoute('property.show', [
+                'id' => $property->getId(),
+                'slug' => $property->getSlug()
+            ], 301);
+        }
+
+        return $this->render('property/show.html.twig', [
+            'current_menu' => 'properties',
+            'property' => $property
+        ]);
+    }
+}
